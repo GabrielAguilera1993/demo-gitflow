55a5e46f4e88d64570dbc88f489bb355e2877319
GabrielAguilera1993 <82842821+GabrielAguilera1993@users.noreply.github.com>
2022-03-04 15:43:43 -0500

    Update App.java

--- src/main/java/demo/workflow/App.java
+++ src/main/java/demo/workflow/App.java
@@ -7,12 +7,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World! test1 delete");
         System.out.println("test2");
-        System.out.println("test2");
         System.out.println("test3");
-        System.out.println("test3");
-        System.out.println("test3");
-        System.out.println("test3");
-
+    
 
     }
 }
