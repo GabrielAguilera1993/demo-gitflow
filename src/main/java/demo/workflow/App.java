e94660c69f84fdc57d903676ff23ccca91f5c6eb
GabrielAguilera1993 <gabriel.m@lakatuna.com>
2022-03-04 20:17:40 +0100

    update line

--- src/main/java/demo/workflow/App.java
+++ src/main/java/demo/workflow/App.java
@@ -8,6 +8,7 @@ public class App {
         System.out.println("Hello World!");
         System.out.println("add new line");
         System.out.println("3er new line update");
+        System.out.println("4ta new line update");
 
 
     }
