6f15d401f2ffc9c2e259cc47fb22bf081b1df7f0
GabrielAguilera1993 <82842821+GabrielAguilera1993@users.noreply.github.com>
2022-03-04 14:10:57 -0500

    Update App.java

--- src/main/java/demo/workflow/App.java
+++ src/main/java/demo/workflow/App.java
@@ -7,7 +7,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("add new line");
-        System.out.println("3er new line");
+        System.out.println("3er new line update");
 
 
     }
